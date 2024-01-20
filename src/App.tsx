e69dd8d1cf5cbb5a19f4ce68c0a6a8300fdba917
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-20 23:37:32 +0600

    added container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import MainLayout from "./componants/layout/MainLayout";
+import Container from "./componants/ui/Container";
 
 function App() {
   return (
-    <>
-      <MainLayout />;
-    </>
+    <Container>
+      <div className="h-screen w-full flex justify-center items-center">
+        <h1>Main Contant</h1>
+        <h1>Main Contant</h1>
+        <h1>Main Contant</h1>
+        <h1>Main Contant</h1>
+        <h1>Main Contant</h1>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/componants/ui/Container.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+const Container = ({ children }: { children: ReactNode }) => {
+  return <div className="h-full w-full mx-auto max-w-[1250px]">{children}</div>;
+};
+
+export default Container;
