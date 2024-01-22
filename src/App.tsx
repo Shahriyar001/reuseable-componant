d0db84c2eb6c730f8232c3c4cc3aedf3987e8365
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-22 17:41:45 +0600

    complite modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Button from "./componants/ui/Button";
 import Container from "./componants/ui/Container";
 import Modal from "./componants/ui/Modal";
@@ -10,13 +10,28 @@ function App() {
     setModal((prev) => !prev);
   };
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("click");
+    if (true) {
+      handleModalClose();
+    }
+  };
+
   return (
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
         <div className=" w-96 border border-red-400 p-10">
           <Button onClick={() => setModal((prev) => !prev)}>Open Modal</Button>
           <Modal isOpen={modal} onClose={handleModalClose}>
-            This is a Modal
+            <Modal.Header>
+              <h1>This is modal title</h1>
+              <Modal.CloseButton></Modal.CloseButton>
+            </Modal.Header>
+            <form onSubmit={handleSubmit}>
+              <input type="text" name="" id="" />
+              <button type="submit">Submit</button>
+            </form>
           </Modal>
         </div>
       </div>
--- src/componants/ui/Modal.tsx
+++ src/componants/ui/Modal.tsx
@@ -1,33 +1,98 @@
-import { useRef } from "react";
+import {
+  Children,
+  MouseEvent,
+  ReactNode,
+  createContext,
+  useContext,
+  useRef,
+} from "react";
 import cn from "../../utils/cn";
 import { createPortal } from "react-dom";
 
-const Modal = ({ isOpen, onClose, children }) => {
+const ModalContext = createContext<TModalContext | null>(null);
+
+type TModalContext = {
+  onClose: () => void;
+};
+
+type TModal = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+type TCloseButton = {
+  children?: ReactNode;
+};
+
+type THeader = TCloseButton;
+
+const Modal = ({ isOpen, onClose, children }: TModal) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const handleOutsideClose = (e) => {
+  const handleOutsideClose = (e: MouseEvent) => {
     console.log(e.target);
-    if (!containerRef?.current?.contains(e.target)) {
+    if (!containerRef?.current?.contains(e.target as Node)) {
       onClose();
     }
   };
 
   return createPortal(
-    <div
-      className={cn(
-        "fixed inset-0 flex justify-center items-center bg-gray-500/70 invisible z-[999]",
-        {
-          visible: isOpen,
-        }
-      )}
-      onClick={handleOutsideClose}
-    >
-      <div ref={containerRef} className="bg-white w-full max-w-sm rounded-md">
-        {children}
+    <ModalContext.Provider value={{ onClose }}>
+      <div
+        className={cn(
+          "fixed inset-0 flex justify-center items-center bg-gray-500/70 invisible z-[999]",
+          {
+            visible: isOpen,
+          }
+        )}
+        onClick={handleOutsideClose}
+      >
+        <div
+          ref={containerRef}
+          className="bg-white w-full max-w-sm rounded-md p-4"
+        >
+          {children}
+        </div>
       </div>
-    </div>,
+      ,
+    </ModalContext.Provider>,
     document.getElementById("portal") as Element
   );
 };
+
+const CLoseButton = ({ children }: TCloseButton) => {
+  const { onClose } = useContext(ModalContext) as TModalContext;
+
+  return (
+    <button className="ml-auto" onClick={onClose}>
+      {children ? (
+        children
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={3}
+          stroke="currentColor"
+          className="w-6 h-6 p-0.5 bg-red-500 rounded-md text-white"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18 18 6M6 6l12 12"
+          />
+        </svg>
+      )}
+    </button>
+  );
+};
+
+const Header = ({ children }: THeader) => {
+  return <div className="flex justify-center w-full mb-2">{children}</div>;
+};
+
+Modal.Header = Header;
+Modal.CloseButton = CLoseButton;
 
 export default Modal;
