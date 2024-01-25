6b38606489f3ad222c14b40f585ed957724cd417
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-25 11:00:18 +0600

    added react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
-import { FormEvent, useState } from "react";
-import Button from "./componants/ui/Button";
 import Container from "./componants/ui/Container";
-import Modal from "./componants/ui/Modal";
+import NormalForm from "./componants/NormalForm/NormalForm";
 
 function App() {
-  const [modal, setModal] = useState(false);
-
-  const handleModalClose = () => {
-    setModal((prev) => !prev);
-  };
-
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    console.log("click");
-    if (true) {
-      handleModalClose();
-    }
-  };
-
   return (
     <Container>
-      <div className="h-screen w-full flex justify-center items-center">
-        <div className=" w-96 border border-red-400 p-10">
-          <Button onClick={() => setModal((prev) => !prev)}>Open Modal</Button>
-          <Modal isOpen={modal} onClose={handleModalClose}>
-            <Modal.Header>
-              <h1>This is modal title</h1>
-              <Modal.CloseButton></Modal.CloseButton>
-            </Modal.Header>
-            <form onSubmit={handleSubmit}>
-              <input type="text" name="" id="" />
-              <button type="submit">Submit</button>
-            </form>
-          </Modal>
-        </div>
-      </div>
+      <NormalForm />
     </Container>
   );
 }
