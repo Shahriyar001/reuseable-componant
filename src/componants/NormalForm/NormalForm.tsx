5846f50278c491662451708303c3669a306a6db7
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-01-25 16:55:11 +0600

    added clsx in reuseable form

--- src/componants/NormalForm/NormalForm.tsx
+++ src/componants/NormalForm/NormalForm.tsx
@@ -1,16 +1,69 @@
 import { useForm } from "react-hook-form";
+import cn from "../../utils/cn";
 
 const NormalForm = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
     console.log(data);
   };
+  const double = true;
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" {...register("name")} />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={cn("border border-red-500 w-full p-5 mx-auto", {
+        "max-w-5xl": double,
+        "max-w-md": !double,
+      })}
+    >
+      <div
+        className={cn(
+          "border border-blue-500 grid grid-cols-1 justify-items-center  gap-5",
+          {
+            "md:grid-cols-2": double,
+          }
+        )}
+      >
+        <div className="w-full max-w-md">
+          <label className="block" htmlFor="name">
+            Name
+          </label>
+          <input
+            type="text"
+            className="w-full border border-gray-300 rounded-md focus:border-purple-500 focus:ring-1 focus:ring-purple-500"
+            id="name"
+            {...register("name")}
+          />
+        </div>
+        <div className="w-full max-w-md">
+          <label className="block" htmlFor="name">
+            email
+          </label>
+          <input
+            className="w-full"
+            type="email"
+            id="email"
+            {...register("email")}
+          />
+        </div>
+        <div className="w-full max-w-md">
+          <label className="block" htmlFor="name">
+            password
+          </label>
+          <input
+            className="w-full"
+            type="password"
+            id="password"
+            {...register("password")}
+          />
+        </div>
       </div>
+      {/* <select>
+        <option>M</option>
+        <option>L</option>
+        <option>XL</option>
+        <option>XXL</option>
+      </select> */}
+
       <button type="submit">Submit</button>
     </form>
   );
